a18ba691ed22a51d5850d747fda6b887356361da
Tatyana Rezunenko <tanya.kamyshnikova@gmail.com>
2021-04-02 17:01:35 +0300

    first commit

---
+++ scripts/scriptHard.js
@@ -0,0 +1,16 @@
+let num = 266219;
+let str = String(num);
+let arr = [];
+
+for (let i = 0; i < str.length;i++) {
+  arr.push(str[i]);
+}
+//console.log(arr.join());
+
+let multiplication = arr.reduce((acc, index) =>  acc * (+index), 1)
+console.log('multiplication: ', multiplication);
+
+let exponentiation = multiplication ** 3;
+let result = exponentiation.toString().substring(0, 2);
+console.log('result: ', result);
+
