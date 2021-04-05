fd33c409b0ebb80c81e29e639a49721a4177b998
Tatyana Rezunenko <tanya.kamyshnikova@gmail.com>
2021-04-05 18:20:51 +0300

    lessons03Hard

--- scripts/scriptHard.js
+++ scripts/scriptHard.js
@@ -15,3 +15,35 @@ let exponentiation = multiplication ** 3;
 let result = exponentiation.toString().substring(0, 2);
 console.log('result: ', result);
 
+//lessons03
+let lang = prompt('введите язик', 'en or ru').trim().toLowerCase();
+let dayRu = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Субота', 'Воскресенье'];
+let dayEn = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+ 
+if (lang === 'ru') {
+  console.log(dayRu);
+} else if (lang === 'en') {
+  console.log(dayEn);
+}
+
+switch (lang) {
+  case 'ru':
+    console.log(dayRu);
+    break;
+  case 'en':
+    console.log(dayEn);
+    break;
+    default:
+    alert( "Вы ввели не тот язык" );
+}
+
+let langArray = [];
+langArray.ru = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Субота', 'Воскресенье'];
+langArray.en = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+console.log(langArray[lang]);
+
+
+let namePerson = prompt('Введите имя');
+(namePerson === 'Артем') ? console.log("директор")
+  : (namePerson === 'Максим') ? console.log('преподаватель')
+    : console.log('студент');
