bfc0b59ecbaac60b60d136306091d249ead0d495
Tatyana Rezunenko <tanya.kamyshnikova@gmail.com>
2021-04-07 19:27:04 +0300

    lessons05Hard

--- scripts/scriptHard.js
+++ scripts/scriptHard.js
@@ -1,14 +1,14 @@
 "use strict";
 let num = 266219;
 let str = String(num);
-let arr = [];
+let arr1 = [];
 
 for (let i = 0; i < str.length;i++) {
-  arr.push(str[i]);
+  arr1.push(str[i]);
 }
 //console.log(arr.join());
 
-let multiplication = arr.reduce((acc, index) => acc * (+index), 1);
+let multiplication = arr1.reduce((acc, index) => acc * (+index), 1);
 console.log('multiplication: ', multiplication);
 
 let exponentiation = multiplication ** 3;
@@ -49,24 +49,8 @@ let nameLogin = (namePerson === 'Артем') ? "директор" :
   (namePerson === 'Максим') ? 'преподаватель' :
     'студент';
    
-    console.log(nameLogin);
+console.log(nameLogin);
 
-/* function getStringMethod(arg) {
-  if (typeof (arg) !== 'string') {
-   return ("Вы ввели не строку");
-  }  else {
-    let trim = arg.trim();
-     if (trim.length >= 30) {
-      let getString = trim.substring(30);
-      return trim.replace(getString, '...');
-    } 
-    return (trim);
-  } 
-}
-let string = "    . je;rufb     dl/fg/laehglj aenv'ojgef' 'aoefvb'oubef'ou 'eirhfguie      ";
-let resultString = getStringMethod(string);
-console.log('Проверка метода :', resultString );
- */
 function getStringMethod(arg, callBack) {
   if (typeof (arg) !== 'string') {
    return ("Вы ввели не строку");
@@ -81,10 +65,31 @@ function getString(trim) {
   if (trim.length >= 30) {
       let newString = trim.substring(30);
       return trim.replace(newString, '...');
-    } 
+    }
 }
 
 let string = "    . je;rufb     dl/fg/laehglj aenv'ojgef' 'aoefvb'oubef'ou 'eirhfguie      ";
 let resultString = getStringMethod(string, getString);
-console.log('Проверка метода :', resultString );
+console.log('Проверка метода :', resultString);
 
+//lessons 05
+let arr = ['266246', '4538', '1142', '22822', '4852', '548', '8568893'];
+let arr2 = [];
+for (let i in arr) {
+  if (arr[i].substring(0, 1) === '2' || arr[i].substring(0, 1) === '4') {
+    arr2.push(arr[i]) ;
+  }
+}
+console.log(arr2);
+
+let n = 100;
+
+nextPrime:
+for (let i = 2; i <= n; i++) { // Для всех i...
+
+  for (let j = 2; j < i; j++) { // проверить, делится ли число..
+    if (i % j === 0) continue nextPrime; // не подходит, берём следующее
+  }
+
+  console.log(`${i} Делители этого числа: 1 и ${i}`); // простое число
+}
