77a9911175daf84d1611072f8b455607e782846d
Tatyana Rezunenko <tanya.kamyshnikova@gmail.com>
2021-04-10 03:41:47 +0300

    lessons07Hard

--- scripts/scriptHard.js
+++ scripts/scriptHard.js
@@ -1,22 +1,19 @@
 "use strict";
-//lessons 05
-let arr = ['266246', '4538', '1142', '22822', '4852', '548', '8568893'];
-let arr2 = [];
-for (let i in arr) {
-  if (arr[i].substring(0, 1) === '2' || arr[i].substring(0, 1) === '4') {
-    arr2.push(arr[i]) ;
-  }
-}
-console.log(arr2);
 
-let n = 100;
+ let week = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'],
+   toDay = new Date().getDay() - 1;
 
-nextPrime:
-for (let i = 2; i <= n; i++) { // Для всех i...
-
-  for (let j = 2; j < i; j++) { // проверить, делится ли число..
-    if (i % j === 0) continue nextPrime; // не подходит, берём следующее
-  }
-
-  console.log(`${i} Делители этого числа: 1 и ${i}`); // простое число
-}
+ for (let i = 0; i < week.length; i++) {
+   if (i === toDay) {
+     if (week[i] === 'сб' || week[i] === 'вс') {
+       document.write(`<p><b><i>${week[i]}</i></b></p>`);
+     } else {
+       document.write(`<p><b>${week[i]}</b></p>`);
+    }
+   } else if (week[i] === 'сб' || week[i] === 'вс') {
+     document.write(`<p><b>${week[i]}</b></p>`);
+   } else {
+     document.write(`<p>${week[i]}</p>`);
+   }
+ }
+ console.log(week);
