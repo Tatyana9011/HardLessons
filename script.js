0c12d0ae3f9d7be9800bd0a7a5a3eb42c531ed78
Tatyana Rezunenko <tanya.kamyshnikova@gmail.com>
2021-05-06 10:32:44 +0300

    lessons29HardCurs

--- script.js
+++ script.js
@@ -1,52 +1,80 @@
 document.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
-    const output = document.getElementById('output');
+    const submit = document.querySelector('.submit'),
+        selectionValue = document.querySelector('.selection-value'),
+        valueStart = document.querySelector('.value-start'),
+        selectionResult = document.querySelector('.selection-result'),
+        valueEnd = document.querySelector('.value-end'),
+        responseLabel = document.getElementById('response'),
+        errorMessage = 'Что то не так...',
+        successMessage = 'Готово!';
 
-    const getData = (url) => {
-        return new Promise((resolve, reject) => {
-            const request = new XMLHttpRequest();
-            request.open('GET', url);
-            request.setRequestHeader('Content-type', 'application/json');
+    const statusMessage = document.createElement('div');
+    statusMessage.style.cssText = 'font-size: 2rem;   color: blue;';
+    let valueClient = [];
+    //const myKey2 = 'http://api.exchangeratesapi.io/v1/latest?access_key=306632d1029da4c6c3957483b94d31a8';
+    const getData = url => fetch(url);
 
-            request.addEventListener('readystatechange', () => {
-                if (request.readyState !== 4) {
-                    return;
-                }
-                if (request.status === 200) {
-                    const response = JSON.parse(request.responseText);
-                    resolve(response);
-                } else {
-                    reject(request.statusText);
-                }
-            });
-            request.send();
-        })
 
+    selectionValue.addEventListener('change', () => {
+        valueEnd.value = '';
+        responseLabel.textContent = '';
+        const currency = selectionValue.options[selectionValue.selectedIndex].value;
+        if (currency) {
+            valueClient[0] = currency;
+        }
+    })
+    selectionResult.addEventListener('change', () => {
+        valueEnd.value = '';
+        responseLabel.textContent = '';
+        const currency = selectionResult.options[selectionResult.selectedIndex].value;
+        if (currency) {
+            valueClient[1] = currency;
+        }
+    })
+
+
+    const outputData = response => {
+        if (response.status !== 200) {
+            throw new Error('status network not 200');
+        }
+        statusMessage.textContent = successMessage;
+        return response.json();
+    };
+    const errorData = err => {
+        statusMessage.textContent = errorMessage;
+        valueClient.length = 0;
+        console.error(err);
+        return;
     };
 
-    const outputPhotos = (data) => {
-        data.forEach(item => {
-            output.insertAdjacentHTML('beforebegin',
-                ` <h4>${item.title}</h4>
-            <img src="${item.thumbnailUrl}" alt="${item.title}"> `);
-        })
+    const rendetResult = data => {
+        console.log('data: ', data);
+        for (let key in data) {
+            let labelValue = key.split('_');
+            valueEnd.value = (data[key] * valueStart.value).toFixed(2);
+            if (labelValue[0] === 'USD') {
+                responseLabel.textContent = 'Долларов США (USD)';
+            }
+            if (labelValue[0] === 'RUB') {
+                responseLabel.textContent = 'Русских рублей (RUB)';
+            }
+            if (labelValue[0] === 'EUR') {
+                responseLabel.textContent = 'Евро (EUR)';
+            }
+        }
+    }
 
-    };
-    const urlPhotos = 'https://jsonplaceholder.typicode.com/photos';
-    const oneImg = getData(`${urlPhotos}/1`),
-        twoImg = getData(`${urlPhotos}/2`);
-    /*     oneImg
-            .then(outputPhotos)
-            .catch(error => console.log(error));
-        twoImg
-            .then(outputPhotos)
-            .catch(error => console.log(error)); */
-    /* Promise.race([oneImg, twoImg])  //виполняется только один первый отработавший зен
-        .then(outputPhotos)
-        .catch(error => console.log(error)); */
-
-    Promise.all([oneImg, twoImg])  //получаем масив потому на странице он не отображается потому нужно переберать
-        .then(outputPhotos)
-        .catch(error => console.log(error));
-})
+    submit.addEventListener('submit', event => {
+        event.preventDefault();
+        submit.append(statusMessage);
+        if (valueClient.length === 2) {
+            const myKey = `https://free.currconv.com/api/v7/convert?q=${valueClient[0]}_${valueClient[1]}&compact=ultra&apiKey=0fbe59ab8aef333e169f`;
+            getData(myKey)
+                .then(outputData)
+                .catch(errorData)
+                .then(rendetResult);
+        }
+    });
+});
